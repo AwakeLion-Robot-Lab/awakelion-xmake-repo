c44c85a1f71595ea431795b3bacdc89db5352050
siyiovo <2540488682@qq.com>
2025-12-01 00:13:53 +0800

    refine `awakelion-logger`, which version is `v1.2.5`.

--- packages/a/awakelion-logger/xmake.lua
+++ packages/a/awakelion-logger/xmake.lua
@@ -1,13 +1,19 @@
 package("awakelion-logger")
     set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/AwakeLion-Robot-Lab/awakelion-logger")
     set_description("A low-latency, high-throughput and few-dependencies logger for `AwakeLion Robot Lab` project.")
     set_license("Apache-2.0")
+
     add_deps("ixwebsocket v11.4.6")
 
     add_urls("https://github.com/AwakeLion-Robot-Lab/awakelion-logger.git")
 
     add_versions("1.2.5", "6adc04c43715569b65786b5f4eb13de9f37fbe20")
     add_versions("1.2.4", "4cdc771350c2ca7ac635d985b40a4d5a61cc0306")
+
+    on_load(function (package)
+        package:add("includedirs", package:installdir("include"))
+    end)
 
     on_install(function (package)
         local configs = {}
@@ -22,7 +28,7 @@ package("awakelion-logger")
             #include "aw_logger/aw_logger.hpp"
             void test() {
                 auto logger = aw_logger::getLogger("test");
-                if(logger != nullptr) 
+                if(logger != nullptr)
                     AW_LOG_NOTICE(logger, "hello, awakelion-logger!");
             }
         ]]}, {configs = {languages = "c++20"}}))
